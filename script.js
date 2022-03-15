27b22a8127a617d9d6a8b08424d57d83e1d05205
Malte Hernandez <malte.hernandez@norconsult.com>
2022-03-15 14:27:57 +0100

    Added marker

---
+++ script.js
@@ -0,0 +1,10 @@
+
+
+let map = L.map('map').setView([51.505, -0.09], 13);
+const attribution = 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors';
+
+const tileUrl = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
+const tiles = L.tileLayer(tileUrl, {attribution});
+tiles.addTo(map);
+
+L.marker([50.5, 30.5]).addTo(map);
